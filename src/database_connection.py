00f2d66880d7f8562b93f01ad499617085e07357
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2024-04-19 12:16:27 +0300

    Add python-dotenv and config.py

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,9 +1,10 @@
 import os
 import sqlite3
+from config import DATABASE_NAME
 
 dirname = os.path.dirname(__file__)
 
-connection = sqlite3.connect(os.path.join(dirname, "data", "database.sqlite"))
+connection = sqlite3.connect(os.path.join(dirname, "data", DATABASE_NAME))
 connection.row_factory = sqlite3.Row
 
 
