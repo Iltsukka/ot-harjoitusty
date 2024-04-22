4bdc53b68e015b91a73cdb8cad553c9cd5d0a881
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2024-04-22 12:51:53 +0300

    Update config

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ dirname = os.path.dirname(__file__)
 try:
     load_dotenv(dotenv_path=os.path.join(dirname, '..', '.env'))
 except FileNotFoundError:
-    print('failed to load_dotenv')
+    print('make sure that the .env file is in the top level and not in /src')
 
 
 DATABASE_NAME = os.getenv('DATABASE_NAME') or 'database.sqlite'
